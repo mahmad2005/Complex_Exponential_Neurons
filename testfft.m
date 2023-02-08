0a6a75472573d9375ea940f2975802cc80e8fb2b
MARUF AHMAD <m.ahmad2005@gmail.com>
2023-02-08 16:10:06 -0600

    fft added to panel with phi included

--- testfft.m
+++ testfft.m
@@ -4,14 +4,16 @@ t = -pi:pi/Fs:pi-pi/Fs;
 
 Omega_1 = 1; Theta_1 = log(0.5); Phi_1 = pi/2;
 Omega_2 = 1; Theta_2 = log(0.5); Phi_2 = pi/2;
-Omega_3 = 8; Theta_3 = log(1); Phi_3 = pi/2;
-Omega_4 = 12; Theta_4 = log(0.5); Phi_4 = pi/2;
+Omega_3 = 1; Theta_3 = log(0.5); Phi_3 = pi/2;
+Omega_4 = 1; Theta_4 = log(0.5); Phi_4 = pi/2;
 
 %x = cos(2*pi*2*t - pi/4) + sin(2*2*pi*t + pi/2);
-E1 = exp((1i*100*Omega_1.*t)+Theta_1);
-E2 = exp((1i*400*Omega_2.*t)+Theta_2);
+E1 = exp((1i*10*Omega_1.*t)+Theta_1);
+E2 = exp((1i*40*Omega_2.*t)+Theta_2);
+E3 = exp((1i*80*Omega_3.*t)+Theta_3);
+E4 = exp((1i*100*Omega_4.*t)+Theta_4);
 
-x = E1+E2;
+x = E1+E2+E3+E4;
 y = fft(x);
 z = fftshift(y);
 
@@ -28,4 +30,4 @@ plot(t,x);
 figure;
 absZ = abs(z)/100;
 %plot(f(51:150),absZ(5001:5100));
-plot(f,absZ);
+plot(f(900:1200),absZ(900:1200));
